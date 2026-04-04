fd89947e1c1c88cb3090588b00497bb438f2d176
onu81 <wjdrnsaos@gmail.com>
2026-04-04 20:57:55 +0900

    Ver.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    // 파이어베이스 연동을 위해 플러그인 추가
     id("com.google.gms.google-services")
 }
 
@@ -13,9 +12,9 @@ android {
         applicationId = "com.junhwi.tilecalc"
         minSdk = 24
         targetSdk = 35
-        // 배포를 위해 versionCode와 versionName을 업데이트합니다. (HTML v6.2 반영)
-        versionCode = 16
-        versionName = "6.2"
+        // 중복 오류를 방지하기 위해 버전 코드를 17로 업데이트합니다.
+        versionCode = 17
+        versionName = "6.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -45,7 +44,6 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     
-    // 파이어베이스 BOM 및 Analytics 추가
     implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     implementation("com.google.firebase:firebase-analytics")
 
